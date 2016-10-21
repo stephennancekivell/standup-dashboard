271e9a0bd3bb1a88a731e3c163f91a20edbbe609
Stephen Nancekivell <stephen.nancekivell@macquarie.com>
2016-10-22 10:55:22 +1100

    loading files within the classpath

--- api/src/main/scala/com/stephenn/standup/App.scala
+++ api/src/main/scala/com/stephenn/standup/App.scala
@@ -9,10 +9,11 @@ import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.model.StatusCodes._
-
 import com.amazonaws.regions.{Region, Regions}
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient
 import com.amazonaws.services.dynamodbv2.document.{DynamoDB, Item}
+
+import scala.io.Source
 
 object WebApp extends App {
   implicit val system = ActorSystem("my-system")
@@ -20,9 +21,14 @@ object WebApp extends App {
   // needed for the future flatMap/onComplete in the end
   implicit val executionContext = system.dispatcher
 
-  def getPath(p: String) = {
-    println("get path "+p)
-    Option(ClassLoader.getSystemResource("static/" + p)).map(p => Paths.get(p.toURI))
+  def readFile(p: String): Option[String] = {
+    val path = p.startsWith("/") match {
+      case true => "/static" + p
+      case false => "/static/" + p
+    }
+    println("reading "+path)
+    val stream = Option(getClass.getResourceAsStream(path))
+    stream.map(Source.fromInputStream(_).getLines().mkString)
   }
 
   private def getExtensions(fileName: String) : String = {
@@ -44,20 +50,17 @@ object WebApp extends App {
       entity(as[HttpRequest]) { requestData =>
         complete {
           val fullPath = requestData.uri.path.toString match {
-            case "/" | "" => getPath("index.html")
-            case _ => getPath(requestData.uri.path.toString)
+            case "/" | "" => "index.html"
+            case _ => requestData.uri.path.toString
           }
 
-          def onlyFixed(m: MediaType): Option[MediaType.WithFixedCharset] = m match {
-            case t: MediaType.WithFixedCharset => Some(t)
-            case _ => None
-          }
+          val maybeContent = readFile(fullPath)
 
-          fullPath match {
+          maybeContent match {
             case None =>
               HttpResponse(NotFound)
-            case Some(foundPath) =>
-              val ext = getExtensions(foundPath.getFileName.toString)
+            case Some(content) =>
+              val ext = getExtensions(requestData.uri.path.toString)
               val maybeContentType = MediaTypes.forExtensionOption(ext)
                 .flatMap {
                   case t: MediaType.WithFixedCharset => Some(ContentType(t))
@@ -67,8 +70,7 @@ object WebApp extends App {
                     None
                 }
 
-              val byteArray = Files.readAllBytes(foundPath)
-              val entity = maybeContentType.map(HttpEntity(_, byteArray)).getOrElse(HttpEntity(byteArray))
+              val entity = maybeContentType.map(HttpEntity(_, content)).getOrElse(HttpEntity(content))
               HttpResponse(OK, entity = entity)
           }
         }
@@ -76,7 +78,6 @@ object WebApp extends App {
     }
 
   val logger = Logging(system, getClass)
-
 
   val bindingFuture = Http().bindAndHandle(route, AppConfig.http.interface, AppConfig.http.port)
 
